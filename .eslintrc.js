4c6f0b8f5b5df467ded2c9d43f58b00946ffcfe8
antgioia <antgioia10@gmail.com>
2023-05-24 12:27:50 +0200

    generate vscode extension

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-use-before-define': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions': 0,
+    'linebreak-style': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts'],
+      },
+    },
+  },
+};
---
+++ src/extension.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-unresolved
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {}
+
+export function deactivate() {}
